22db17d5afd99b395e9cc3de0ca576182d0293ad
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2025-08-31 05:49:08 +0530

    DAY 19
    turtle race!, bet for any turtle.

---
+++ DAY 19 turtle race, using object oriented programming and turtle graphics.py
@@ -0,0 +1,41 @@
+from turtle import Turtle,Screen
+from turtle import Turtle,Screen
+import random
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
+colors = ["red","green","blue","yellow","orange","light blue"]
+y_positions = [100,60,20,-20,-60,-100]
+all_turtles = []
+
+
+for turtle_index in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-240,y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on =False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! the {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! the {winning_color} turtle is the winner")
+
+        random_step = random.randint(0,10)
+        turtle.forward(random_step)
+
+
+
+
+
+screen.exitonclick()
