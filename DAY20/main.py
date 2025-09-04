30f12ef4e029ae9a110c6d018d7f94b301c09091
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2025-09-04 06:35:09 +0530

    Add files via upload

---
+++ DAY20/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) <15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor()< -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280:
+        scoreboard.game_over()
+        game_is_on = False
+
+    for turtle in snake.turtles[1:] :
+       
+       if snake.head.distance(turtle)<10:
+           scoreboard.game_over()
+           game_is_on = False
+screen.exitonclick()
